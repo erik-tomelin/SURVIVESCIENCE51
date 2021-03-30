0236b95781924e9a30dd227e3a2b89f59b6825bb
Erik Tomelin <63420907+erik-tomelin@users.noreply.github.com>
2021-03-30 00:11:36 -0300

    Assets

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour {
+
+    Player Player;
+    
+    // Start is called before the first frame update
+    void Start() {
+        Player = gameObject.transform.parent.gameObject.GetComponent<Player>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collisor) {
+        if(collisor.gameObject.layer == 8) {
+            Player.isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collisor) {
+        if(collisor.gameObject.layer == 8) {
+            Player.isJumping = true;
+        }
+    }
+}
